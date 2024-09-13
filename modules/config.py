0b0deb7ba8a8918cfad6d65851af70c6f1f705ab
Txtdatboy <herokukeliy@outlook.com>
2024-09-13 11:02:31 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -3,4 +3,4 @@ BOT_NAME = "Hardikpanddyabot"
 BOT_TOKEN = "7178616368:AAH2i_xOGJO9bNwpG_yLjkljxpbVFUWUZHg"
 API_ID = 28328736
 API_HASH = "802254a44896baa87f3083b7af36b2e5"
-MONGO_URI = ""
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
