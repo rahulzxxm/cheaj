a21090393d23206a5d9f73f1c0bc82f4e623c298
Txtdatboy <herokukeliy@outlook.com>
2024-09-13 10:34:32 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "Hardikpanddyabot"
+BOT_TOKEN = "7178616368:AAH2i_xOGJO9bNwpG_yLjkljxpbVFUWUZHg"
+API_ID = 28328736
+API_HASH = "802254a44896baa87f3083b7af36b2e5"
+MONGO_URI = "mongodb+srv://KunalUser:9050306314@lakshaykunal.k8d5iac.mongodb.net/?retryWrites=true&w=majority&appName=LakshayKunal"
