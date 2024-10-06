cbc5d20a7efcb5da4e404a5ad9e4c585f16cdc1b
Txtdatboy <herokukeliy@outlook.com>
2024-10-07 02:47:54 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "Hardikpanddyabot"
-BOT_TOKEN = "7178616368:AAH2i_xOGJO9bNwpG_yLjkljxpbVFUWUZHg"
+BOT_NAME = "Free bot"
+BOT_TOKEN = "6755775439:AAGkahjp3xK71u-jG6V0uQUR-xJgqLPt9yw"
 API_ID = 28328736
 API_HASH = "802254a44896baa87f3083b7af36b2e5"
 MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
